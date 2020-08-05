e076e150ba3e44861d2363e44b4246253a543424
gruppi <mauricio.gruppi@gmail.com>
2020-08-05 14:05:26 -0400

    Add image functionality

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
 from flask import Flask, render_template, request
 import json
 import os
+import numpy as np
 from datetime import datetime
 import requests
 from requests.auth import HTTPBasicAuth
+
+from WordVectors import WordVectors, intersection
+from alignment import align
+import plot
+
 app = Flask(__name__)
+app.config["IMAGE_FOLDER"] = os.path.join("static", "image")
+
+
+def distribution_of_change(*wvs, metric="euclidean"):
+    """
+    Gets distribution of change per word across input WordVectors list wvs.
+    Assumes the WordVectors in wvs have been previously aligned to the same reference point
+    (E.g.: align all to wvs[0]).
+    Arguments:
+            wvs - list of WordVectors objects
+    Returns:
+            d   - array of N elements with the mean cosine distance across the aligned WordVectors
+                    (N is the size of the common vocabulary)
+    """
+
+    d = np.zeros((len(wvs[0])))
+    for i, w in enumerate(wvs[0].words):
+        # Compute mean vector
+        v_mean = np.mean([wv[w] for wv in wvs], axis=0)
+        # Compute distances to the mean
+        if metric == "euclidean":
+            distances = [np.linalg.norm(v_mean-wv[w])**2 for wv in wvs]
+        elif metric == "cosine":
+            distances = [cosine(v_mean, wv[w]) for wv in wvs]
+        # distances = [cosine(v_mean, wv[w]) for wv in wvs]
+        mean_d = np.mean(distances)
+        d[i] = mean_d
+    return d
+
+
+def read_string(s):
+    # Use this function to process line reading in map
+    def process_line(s):
+        s = s.rstrip().split(b' ', 1)
+        w = s[0]
+        v = np.array(s[1].split(b' '), dtype=float)
+        return w.decode("utf-8"), v
+
+    lines = s.strip().split(b'\n')
+    lines = lines[1:]
+    data = map(process_line, lines)
+    words, vectors = zip(*data)
+    return words, vectors
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -20,8 +69,39 @@ def index():
         print(request.files["input1"])
         print(request.files["input2"])
 
-        print(request.files["input1"].read())
-        return render_template("demo.html", data=None)
+        f1 = request.files["input1"].read()
+        words, vectors = read_string(f1)
+
+        wv1 = WordVectors(words=words, vectors=vectors)
+
+        f2 = request.files["input2"].read()
+        words, vectors = read_string(f2)
+        wv2 = WordVectors(words=words, vectors=vectors)
+
+        wv1, wv2 = intersection(wv1, wv2)
+        wv1, wv2, _ = align(wv1, wv2)
+        words = wv1.words
+        print("Finished reading")
+        print(len(wv1), "words")
+
+
+        d = distribution_of_change(wv1, wv2)
+        words_sorted = np.argsort(d)[::-1]
+
+        top_words = [words[i] for i in words_sorted[:40]]
+
+        img_path = os.path.join(app.config["IMAGE_FOLDER"], "tmp.png")
+        plot.plot_words(wv1, wv2, top_words[:10], img_path)
+
+        # Store result data
+        data = dict()
+        data["common"] = len(wv1)
+        data["filename1"] = request.files["input1"].filename
+        data["filename2"] = request.files["input2"].filename
+
+        return render_template("demo.html", data=data,
+                                img=img_path,
+                                words=top_words)
 
 
 
