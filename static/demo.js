761188bad813134b288fb398be2fb3ac8e7457b1
mgruppi <mauricio.gruppi@gmail.com>
2021-05-24 17:12:53 -0400

    New interface is put together.

--- static/demo.js
+++ static/demo.js
@@ -1,12 +1,19 @@
 function openTab(evt, target)
 {
-    var i, x, tablinks;
+    var i, x;
+    var tgt_id = target.split("-")[1]-1;
     x = document.getElementsByClassName("content-page");
     for (i=0; i < x.length; ++i)
     {
         x[i].style.display = "none";
-        // Set inactive
     }
+
+    var tabs = document.getElementsByClassName("nav-link");
+    for (i=0; i < tabs.length; ++i)
+    {
+        tabs[i].classList.remove("active");
+    }
+
+    tabs[tgt_id].classList.add("active");
     document.getElementById(target).style.display = "block";
-    // Set evt target as active and other inactive.
 }
