b1eb719a63df609ee2a3d87862f5c3f436105764
mgruppi <mauricio.gruppi@gmail.com>
2021-08-08 14:57:42 -0400

    Add script to run all embeddings

--- preprocessing/run_embeddings.sh
+++ preprocessing/run_embeddings.sh
@@ -11,10 +11,10 @@ do
   echo "$f"
   for g in $in_path/$f/c1/*
   do
-    python3 embedding.py "$g" "$out_path/embeddings/$f/c1.vec"
+    python3 embedding.py "$g" "$out_path/$f/c1.vec"
   done
   for g in $in_path/$f/c2/*
   do
-    python3 embedding.py "$g" "$out_path/embeddings/$f/c2.vec"
+    python3 embedding.py "$g" "$out_path/$f/c2.vec"
   done
 done
