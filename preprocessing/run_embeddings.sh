c8042ea570b046e6c8f5370992c80f0271019780
mgruppi <mauricio.gruppi@gmail.com>
2021-08-08 14:54:30 -0400

    Add script to run all embeddings

---
+++ preprocessing/run_embeddings.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+in_path="../../s4/corpus"
+out_path="../../s4/embeddings"
+datasets=(hist-english hist-german hist-latin hist-swedish ukus)
+
+
+for f in ${datasets[@]}
+do
+  mkdir -p "$out_path/$f"
+  echo "$f"
+  for g in $in_path/$f/c1/*
+  do
+    python3 embedding.py "$g" "$out_path/embeddings/$f/c1.vec"
+  done
+  for g in $in_path/$f/c2/*
+  do
+    python3 embedding.py "$g" "$out_path/embeddings/$f/c2.vec"
+  done
+done
