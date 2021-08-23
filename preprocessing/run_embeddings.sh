f16483b31ed540ae043355a36f2529dd9e75a1fb
mgruppi <mauricio.gruppi@gmail.com>
2021-08-23 17:01:58 -0400

    Add arguments for w2v params.

--- preprocessing/run_embeddings.sh
+++ preprocessing/run_embeddings.sh
@@ -4,6 +4,11 @@ in_path="../../s4/corpus"
 out_path="../../s4/embeddings"
 datasets=(hist-english hist-german hist-latin hist-swedish ukus)
 
+#w2v params
+vector_size=100
+window=10
+min_count=50
+
 
 for f in ${datasets[@]}
 do
@@ -11,10 +16,16 @@ do
   echo "$f"
   for g in $in_path/$f/c1/*
   do
-    python3 embedding.py "$g" "$out_path/$f/c1.vec"
+    python3 embedding.py "$g" "$out_path/$f/c1.vec" \
+      --vector_size="$vector_size" \
+      --window="$window" \
+      --min_count="$min_count"
   done
   for g in $in_path/$f/c2/*
   do
-    python3 embedding.py "$g" "$out_path/$f/c2.vec"
+    python3 embedding.py "$g" "$out_path/$f/c2.vec" \
+      --vector_size="$vector_size" \
+      --window="$window" \
+      --min_count="$min_count"
   done
 done
