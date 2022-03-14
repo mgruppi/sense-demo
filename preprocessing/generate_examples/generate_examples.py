0ab5f264f1b57303c600a89dd56d5a646b19cbc6
Brian Hotopp <brihoto@gmail.com>
2022-03-14 14:21:48 -0400

    added example configuration object to each examples object in examples_config.json

--- preprocessing/generate_examples/generate_examples.py
+++ preprocessing/generate_examples/generate_examples.py
@@ -1,3 +1,16 @@
 import argparse
 import argparse
 import json
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path_to_config", type=str, help="Path to configuration file for example generation")
+    args = parser.parse_args()
+    with open("metadata/examples_config.json") as examples_config_file:
+        examples_list = json.loads(examples_config_file.read())
+    # crawl each example and generate it if necessary
+    for example in examples_list:
+        if "example_id" not in example:
+            # we should generate this example again
+
+if __name__ == "__main__":
+    main()
